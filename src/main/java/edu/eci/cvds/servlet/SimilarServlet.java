6a82a613bdd39f1ffd0798077ba819dc5130545b
anfegoca <andres.gonzalez-ca@linux-61.labinfo.is.escuelaing.edu.co>
2020-02-26 10:53:27 -0500

    Punto 12

--- src/main/java/edu/eci/cvds/servlet/SimilarServlet.java
+++ src/main/java/edu/eci/cvds/servlet/SimilarServlet.java
@@ -5,8 +5,13 @@
  */
 package edu.eci.cvds.servlet;
 
+import edu.eci.cvds.servlet.model.Todo;
 import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Optional;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,10 +20,29 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author andres.gonzalez-ca
  */
-
+@WebServlet(
+    urlPatterns = "/Andres"
+)
 public class SimilarServlet extends  HttpServlet {
+    private ArrayList<Todo> todos = new ArrayList<Todo>();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Writer responseWriter = resp.getWriter();
+        Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
+        String name = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
+        try{
+            int id = Integer.parseInt(name);
+            Todo todo = Service.getTodo(id);
+            todos.add(todo);
+            resp.setStatus(HttpServletResponse.SC_OK);
+            responseWriter.write(Service.todosToHTMLTable(todos));
+            responseWriter.flush();
+            
+        }catch(Exception e){
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        
+        
         
     }
 }
